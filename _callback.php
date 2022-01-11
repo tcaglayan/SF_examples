7b22fdacbf364edba376c57686c3d2a02d9f46f3
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-01-11 21:43:00 +1100

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="salesforce-community" content="https://<?php echo getenv('talia-developer-edition.ap24.force.com');?>">
+    <meta name="salesforce-community" content="https://<?php echo getenv('talia-developer-edition.ap24.force.com/TCLogin');?>">
 	<meta name="salesforce-mode" content="<?php echo getenv('modal');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
-	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>">
-	<script src="https://talia-developer-edition.ap24.force.com/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+	<meta name="salesforce-allowed-domains" content="<?php echo getenv('simplesampletee.herokuapp.com');?>">
+	<script src="https://talia-developer-edition.ap24.force.com/TCLogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 </head> 
 <body></body>    
 </html>
