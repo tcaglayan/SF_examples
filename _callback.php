1c996ba722ab8bab840cedfe9b69bd2507ad2019
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-01-12 09:56:19 +1100

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -6,6 +6,17 @@
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('simplesampletee.herokuapp.com');?>">
 	<script src="https://talia-developer-edition.ap24.force.com/TCLogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+	
+	  <meta name="salesforce-community" content="https://talia-developer-edition.ap24.force.com/TCLogin">
+    <meta name="salesforce-client-id" content="3MVG9fe4g9fhX0E5QbeE1lke8xjJgNL0AjDGYnf1lPafEBcIbZCKJA.yCe9RiAgHu.otEJjC6h0SqCS9yQQWh">
+	
+    <meta name="salesforce-target" content="#sign-in-link">
+    <meta name="salesforce-save-access-token" content="true">
+    <meta name="salesforce-forgot-password-enabled" content="true">
+    <meta name="salesforce-self-register-enabled" content="true">
+    <meta name="salesforce-login-handler" content="onLogin">
+    <meta name="salesforce-logout-handler" content="onLogout">
+    <meta name="salesforce-mask-redirects" content="false">
 </head> 
 <body></body>    
 </html>
