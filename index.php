df8e22535e028f947d5c0fa5b4e6820abb6c7678
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-01-11 17:06:05 +1100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <meta name="salesforce-community" content="https://<?php echo getenv('talia-developer-edition.ap24.force.com/TCLogin');?>">
     <meta name="salesforce-client-id" content="<?php echo getenv('3MVG9fe4g9fhX0E5QbeE1lke8xjJgNL0AjDGYnf1lPafEBcIbZCKJA.yCe9RiAgHu.otEJjC6h0SqCS9yQQWh');?>">
     <meta name="salesforce-redirect-uri" content="https://<?php echo getenv('simplesampletee.herokuapp.com');?>/_callback.php">
-    <meta name="salesforce-mode" content="<?php echo getenv('modal-callback');?>">
+    <meta name="salesforce-mode" content="<?php echo getenv('modal');?>">
     <meta name="salesforce-namespace" content="<?php echo getenv('https://mindful-badger-wazuaj-dev-ed.lightning.force.com');?>">
     <meta name="salesforce-target" content="#sign-in-link">
     <meta name="salesforce-save-access-token" content="true">
