017366bb91b0c4963a84b49744d85fb627fb8e8b
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-02-07 06:16:22 +1100

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <meta name="salesforce-mode" content="modal">
 	<meta name="salesforce-namespace" content="<?php echo getenv('talia-developer-edition.ap24.force.com');?>">
 
-    <meta name="salesforce-target" content="#sign-in-link">
+    <meta name="salesforce-target" content="https://talia-developer-edition.ap24.force.com/TCLogin">
     <meta name="salesforce-save-access-token" content="true">
     <meta name="salesforce-forgot-password-enabled" content="true">
     <meta name="salesforce-self-register-enabled" content="true">
@@ -127,123 +127,12 @@
 
 	
 	<script>
-
-
 	function onLogin(identity) {
-		
 		alert(JSON.stringify(identity));
-		
-		var targetDiv = document.querySelector(SFIDWidget.config.target);	
-		
-		var avatar = document.createElement('a'); 
-	 	avatar.href = "javascript:showIdentityOverlay();";
-		
-		
-		var img = document.createElement('img'); 
-	 	img.src = identity.photos.thumbnail; 
-		img.className = "sfid-avatar";
-	
-		var username = document.createElement('span'); 
-		username.innerHTML = identity.username;
-		username.className = "sfid-avatar-name";
-	
-		var iddiv = document.createElement('div'); 
-	 	iddiv.id = "sfid-identity";
-		
-		avatar.appendChild(img);
-		avatar.appendChild(username);
-		iddiv.appendChild(avatar);		
-	
-		targetDiv.innerHTML = '';
-		targetDiv.appendChild(iddiv);	
-		
-		var aero = document.getElementById("aero_link");
-		aero.href = "/datasheets/AeroPress-Instr-English-Rev.-D2.pdf";
-		aero.innerHTML = 'Datasheet';
-
-		var reactor = document.getElementById("reactor_link");
-		reactor.href = "/datasheets/Reactor_StovInst_EURO_EN.pdf";
-		reactor.innerHTML = 'Datasheet';
-
-		var chemex = document.getElementById("chemex_link");
-		chemex.href = "/datasheets/2014_ChemexBrewGuide.pdf";
-		chemex.innerHTML = 'Datasheet';
-		
 	}
-	
-	
-	function showIdentityOverlay() {
-
-		var lightbox = document.createElement('div'); 
-	 	lightbox.className = "sfid-lightbox";
-	 	lightbox.id = "sfid-login-overlay";
-		lightbox.setAttribute("onClick", "SFIDWidget.cancel();");
-		
-		var wrapper = document.createElement('div'); 
-	 	wrapper.id = "identity-wrapper";
-		wrapper.onclick = function(event) {
-		    event = event || window.event // cross-browser event
-    
-		    if (event.stopPropagation) {
-		        // W3C standard variant
-		        event.stopPropagation()
-		    } else {
-		        // IE variant
-		        event.cancelBubble = true
-		    }
-		}
-		
-		var content = document.createElement('div'); 
-	 	content.id = "sfid-content";
-
-		var community = document.createElement('a');
-		var commURL = document.querySelector('meta[name="salesforce-community"]').content;
-		community.href = commURL;
-		community.innerHTML = "Go to the Community";
-		community.setAttribute("style", "float:left");
-		content.appendChild(community);
-
-
-		var logout = document.createElement('a'); 
-	 	logout.href = "javascript:SFIDWidget.logout();SFIDWidget.cancel();";
-		logout.innerHTML = "logout";
-		logout.setAttribute("style", "float:right");
-		content.appendChild(logout);
-		
-		var t = document.createElement('div'); 
-	 	t.id = "sfid-token";
-		t.className = "sfid-mb24";
-		
-		var p = document.createElement('pre'); 
-	 	p.innerHTML = JSON.stringify(SFIDWidget.openid_response, undefined, 2);
-		t.appendChild(p);
-		
-		content.appendChild(t);
-
-		
-		wrapper.appendChild(content);
-		lightbox.appendChild(wrapper);
-		
-		document.body.appendChild(lightbox);	
-		
-	}
-	
 	
 	function onLogout() {
 		SFIDWidget.init();
-		
-		var aero = document.getElementById("aero_link");
-		aero.href = "#";
-		aero.innerHTML = 'Login for more info';
-
-		var reactor = document.getElementById("reactor_link");
-		reactor.href = "#";
-		reactor.innerHTML = 'Login for more info';
-
-		var chemex = document.getElementById("chemex_link");
-		chemex.href = "#";
-		chemex.innerHTML = 'Login for more info';
-
 	}
 
 
