a1bf8398c179723135378ab56b3811328ff3f9ed
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-02-07 05:55:44 +1100

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="salesforce-community" content="https://<?php echo getenv('talia-developer-edition.ap24.force.com/TCLogin');?>">
+   
 	<meta name="salesforce-mode" content="modal-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('simplesampletee.herokuapp.com');?>">
@@ -9,14 +9,7 @@
 	<meta name="salesforce-redirect-uri" content="https://simplesampletee.herokuapp.com/_callback.php">
 	  <meta name="salesforce-community" content="https://talia-developer-edition.ap24.force.com/TCLogin">
     <meta name="salesforce-client-id" content="3MVG9fe4g9fhX0E5QbeE1lke8xjJgNL0AjDGYnf1lPafEBcIbZCKJA.yCe9RiAgHu.otEJjC6h0SqCS9yQQWh">
-	
-    <meta name="salesforce-target" content="#sign-in-link">
-    <meta name="salesforce-save-access-token" content="true">
-    <meta name="salesforce-forgot-password-enabled" content="true">
-    <meta name="salesforce-self-register-enabled" content="true">
-    <meta name="salesforce-login-handler" content="onLogin">
-    <meta name="salesforce-logout-handler" content="onLogout">
-    <meta name="salesforce-mask-redirects" content="true">
+
 </head> 
 <body></body>    
 </html>
