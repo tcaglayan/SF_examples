6a712061d4a6140954f968bc09bf2098849fa675
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-02-07 21:55:11 +1100

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -5,10 +5,10 @@
 	<meta name="salesforce-mode" content="modal-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('simplesampletee.herokuapp.com');?>">
-	<script src="https://talia-developer-edition.ap24.force.com/TCLogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+	<script src="https://preprod-birdlifeaustralia.cs74.force.com/birdlifelogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 	<meta name="salesforce-redirect-uri" content="https://simplesampletee.herokuapp.com/_callback.php">
 	  <meta name="salesforce-community" content="https://talia-developer-edition.ap24.force.com/TCLogin">
-    <meta name="salesforce-client-id" content="3MVG9fe4g9fhX0E5QbeE1lke8xjJgNL0AjDGYnf1lPafEBcIbZCKJA.yCe9RiAgHu.otEJjC6h0SqCS9yQQWh">
+    <meta name="salesforce-client-id" content="3MVG9aWdXtdHRrI3dug6SN7vaYLE9TIslqGA4Tgo0y8RTsG6fjOlGgfXiGCfb596pkad7H383hB_ixwo6CsEx">
 
 </head> 
 <body></body>    
