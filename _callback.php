f999cd8430a089a194b110ca59be5ec9793cafd1
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-02-07 22:28:22 +1100

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -1,15 +1,13 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   
-	<meta name="salesforce-mode" content="modal-callback">
+    <meta name="salesforce-community" content="https://preprod-birdlifeaustralia.cs74.force.com/birdlifelogin">
+	<meta name="salesforce-mode" content="<?php echo getenv('modal');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
-	<meta name="salesforce-allowed-domains" content="<?php echo getenv('simplesampletee.herokuapp.com');?>">
+	<meta name="salesforce-allowed-domains" content="simplesampletee.herokuapp.com">
 	<script src="https://preprod-birdlifeaustralia.cs74.force.com/birdlifelogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
-	<meta name="salesforce-redirect-uri" content="https://simplesampletee.herokuapp.com/_callback.php">
-	  <meta name="salesforce-community" content="https://talia-developer-edition.ap24.force.com/TCLogin">
-    <meta name="salesforce-client-id" content="3MVG9aWdXtdHRrI3dug6SN7vaYLE9TIslqGA4Tgo0y8RTsG6fjOlGgfXiGCfb596pkad7H383hB_ixwo6CsEx">
-
 </head> 
 <body></body>    
 </html>
