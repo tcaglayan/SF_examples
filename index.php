4e5ad56569a70fe2800c4c0b702897c7ca37ef5f
tcaglayan <97494338+tcaglayan@users.noreply.github.com>
2022-02-07 21:54:31 +1100

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,600" type="text/css" rel="stylesheet">
     <link href="main.css" rel="stylesheet">
 	
-    <meta name="salesforce-community" content="https://talia-developer-edition.ap24.force.com/TCLogin">
-    <meta name="salesforce-client-id" content="3MVG9fe4g9fhX0E5QbeE1lke8xjJgNL0AjDGYnf1lPafEBcIbZCKJA.yCe9RiAgHu.otEJjC6h0SqCS9yQQWh">
+    <meta name="salesforce-community" content="https://preprod-birdlifeaustralia.cs74.force.com/birdlifelogin">
+    <meta name="salesforce-client-id" content="3MVG9aWdXtdHRrI3dug6SN7vaYLE9TIslqGA4Tgo0y8RTsG6fjOlGgfXiGCfb596pkad7H383hB_ixwo6CsEx">
     <meta name="salesforce-redirect-uri" content="https://simplesampletee.herokuapp.com/_callback.php">
     <meta name="salesforce-mode" content="modal">
 	
@@ -22,8 +22,8 @@
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
     <meta name="salesforce-mask-redirects" content="true">
-	<link href="https://talia-developer-edition.ap24.force.com/TCLogin/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
-    <script src="https://talia-developer-edition.ap24.force.com/TCLogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+	<link href="https://preprod-birdlifeaustralia.cs74.force.com/birdlifelogin/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
+    <script src="https://preprod-birdlifeaustralia.cs74.force.com/birdlifelogin/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
   </head>
   
   <body>
